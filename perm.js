7cff733240b3b8dddab61fccc8a809475f6ab400
Pr!x <50447305+PrIxOwO@users.noreply.github.com>
2021-08-16 03:14:20 +0300

    Add files via upload

---
+++ perm.js
@@ -0,0 +1,8 @@
+//  client.on('message', message=>{
+    if (message.member.roles.cache.has(/*dc id*/)) {
+        //stuf u whant if got perm
+    }
+    else{
+       //no u cant do that
+}
+//  })
